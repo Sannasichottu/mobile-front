e8a016634795d62a8946005eb9719a7f04aa74ff
Sannasichottu <sannasichottu@gmail.com>
2022-04-01 17:42:08 +0530

    mobilee-front

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PhoneList />
     </div>
   );
 }
+
+function PhoneList(){
+  const [ mobiles , setMobiles ] = useState([]);
+
+  useEffect(() =>{
+    fetch('http://localhost:4000/mobiles')
+   .then((data) => data.json())
+   .then((mbs) => setMobiles(mbs));
+  }, [])
+
+  return (
+    <div className="phone-list-container">
+      {mobiles.map(mobile => <Phone key={mobile._id} mobile = {mobile} /> )}
+    </div>
+  );
+}
+
+function Phone({ mobile }){
+
+  return(
+  <div className='phone-container'>
+    <img src={mobile.img} alt={mobile.model} className='phone-picture'/>
+    <h2 className='phone-name'>{mobile.model}</h2>
+    <p className='phone-company'>{mobile.company}</p>
+  </div>
+)}
+
 
 export default App;
